3bfdeb028e3d1c8581ec2bd39baa1c06764b7c99
Andrew Bernier <a6bernier@gmail.com>
2018-09-11 18:46:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return name_hash if name_hash.size <= 1
-  swap = true
-    while swap
-      swap = false
-      (name_hash.length - 1).times do |x|
-        if name_hash[x] > name_hash[x+1]
-          name_hash[x], name_hash[x+1] = name_hash[x+1], name_hash[x]
-          swap = true
-        end
-      end
-    end
-  name_hash
-end
+
 end
